1fb1d02299492e5712e08ba5b6c3c224b040377a
Mateo Galić <mateogalic112@gmail.com>
2022-09-03 15:51:56 +0200

    .

--- utils/constants/index.js
+++ utils/constants/index.js
@@ -2,4 +2,4 @@ export const IPFS_GATEWAY = "https://ipfs.io/ipfs";
 
 export const IPFS_JSON_CID = "QmSzEogKH5QPP1BBmvKTrfK8Ni7LTtN3JCG8iH469u8yDT";
 
-export const CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const CONTRACT_ADDRESS = "0xA23A2C51bd752b0Bdd6C25785024523C105C3c8c";
