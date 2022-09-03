441235a13a7014d1f87c2f269bbc0595828fee20
Mateo Galić <mateogalic112@gmail.com>
2022-09-03 15:21:07 +0200

    .

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,19 +1,19 @@
 require("@nomiclabs/hardhat-waffle");
-require('dotenv').config({path: `${__dirname}/.env`})
+require("dotenv").config({ path: `${__dirname}/.env` });
 
 module.exports = {
   networks: {
     hardhat: {
-      chainId: 1337
+      chainId: 1337,
     },
     mumbai: {
       url: `https://polygon-mumbai.infura.io/v3/${process.env.PROJECT_ID}`,
-      accounts: [process.env.METAMASK_ACCOUNT_KEY]
+      accounts: [process.env.METAMASK_ACCOUNT_KEY],
     },
     mainnet: {
       url: `https://polygon-mainnet.infura.io/v3/${process.env.PROJECT_ID}`,
-      accounts: [process.env.METAMASK_ACCOUNT_KEY]
-    }
+      accounts: [process.env.METAMASK_ACCOUNT_KEY],
+    },
   },
   solidity: "0.8.4",
 };
